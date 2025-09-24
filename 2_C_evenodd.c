7914c857367f994ece676c10e9a5d4abb5cf3746
Saidutta Gundu <saidattagundu15@gmail.com>
2025-09-24 14:37:24 +0530

    Add files via upload

---
+++ 2_C_evenodd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+    int n;
+
+    printf("Enter the no.:");
+    scanf("%d", &n);
+
+    if (n % 2==0)
+    {
+        printf("Even");
+    }
+    else
+    {
+        printf("Odd");
+    }
+}
