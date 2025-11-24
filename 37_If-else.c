8cc8fcbe4bf6c9f9787050b264c178718fa84445
Saidutta Gundu <saidattagundu15@gmail.com>
2025-11-24 23:38:49 +0530

    Add grading system based on percentage input

---
+++ 37_If-else.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int p;
+    printf("Enter percentage: ");
+    scanf("%d",&p);
+
+    if(p>=90)       printf("Grade A+");
+    else if(p>=80)  printf("Grade A");
+    else if(p>=70)  printf("Grade B");
+    else if(p>=60)  printf("Grade C");
+    else if(p>=50)  printf("Grade D");
+    else            printf("Fail");
+}
