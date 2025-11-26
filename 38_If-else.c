95fb32318bb7fdc8168e5836358abe53447cc1e7
Saidutta Gundu <saidattagundu15@gmail.com>
2025-11-26 19:34:58 +0530

    Add if-else program to compare two numbers

---
+++ 38_If-else.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("Enter two numbers: ");
+    scanf("%d%d",&a,&b);
+
+    if(a==b)
+        printf("Equal");
+    else if(a>b)
+        printf("A is larger");
+    else
+        printf("B is larger");
+}
